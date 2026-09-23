f39734360ee37acb560ab21c7f62b0b42096d06e
Kushagra Bharti <kushagrabharti04@gmail.com>
2026-09-23 13:27:27 +0530

    Add chapter_5.py to demonstrate real number calculations

---
+++ 03_data_types/chapter_5.py
@@ -0,0 +1,8 @@
+# Real number
+
+ideal_temp = 95.5;
+current_temp = 95.4999999999
+
+print (f"ideal temp: {ideal_temp}")
+print (f"current temp: {current_temp}")
+print (f"difference: {ideal_temp - current_temp}") # here difference is calculated by subtracting current temp from ideal temp
