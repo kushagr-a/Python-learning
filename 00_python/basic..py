81484356ef333c88aeabdfe73be3f61a7a91e3db
Kushagra Bharti <kushagrabharti04@gmail.com>
2026-09-25 16:56:39 +0530

    Add basic.py and loops_01.py to demonstrate string formatting and loops in Python

---
+++ 00_python/basic..py
@@ -0,0 +1,7 @@
+name = "Kushagra"
+age = 22
+
+print(f"My name is {name} and next year I will be {age + 1}.")
+
+# the use of "f" it's make name and age variable to be used in the string without using + operator
+#  but when u not use "f" then it's treated like a normal string and the variable will not be evaluated and it will be printed as it is.
---
+++ 05_loops/loops_01.py
@@ -0,0 +1,5 @@
+# here we are study about loops in python
+# for loop and range
+
+for token in range(1, 11): # here token is a variable which will take the value from 1 to 10 and 11 is not included
+    print(f"serving chai to token #{token}") # here we are printing the value of token in the string
