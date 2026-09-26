9f2bc768784d7631e750a8ed7714bfa79f7a711c
Kushagra Bharti <kushagrabharti04@gmail.com>
2026-09-26 12:11:49 +0530

    Add functions to fetch, filter, summarize sales data and generate report

---
+++ 06_functions/reducing_code_duplication.py
@@ -0,0 +1,12 @@
+# def is keyword to define a function
+# printOrder is the name of the function
+# pass used to avoid indentation error, it does nothing
+
+
+def printOrder(name, chai_type):  # here passing the parameters in () -> this blocks
+    print(f"Order received for {name} with {chai_type} chai.")
+
+
+
+printOrder("kushagra", "masala")  # here we are passing the arguments in () -> this blocks
+printOrder("Alice", "ginger") 
